0a3494e6ddd1cf69382345231bfeeb7af9ac9556
Raiha Khan <rk482@njit.edu>
2017-08-22 19:34:03 -0400

    Update coal.R

--- coal.R
+++ coal.R
@@ -1,19 +1,17 @@
-# ARIMA MODELING
+# UNIVARIATE PREDICTIVE MODELING
 
-# Indicate which website data was sourced from 
+# Data sourced from https://datamarket.com/data/set/22or/monthly-bituminous-coal-production-in-usa-1952-1959#!ds=22or&display=line
 
 # install.packages('dplyr')
 # install.packages('data.table')
 # install.packages('tseries')
 # install.packages('forecast')
-# install.packages('zoo')
 library(stats)
 library(ggplot2)
 library(dplyr)
 require(graphics)
 require(tseries)
 library(forecast)
-library(zoo)
 library(tseries)
 library(dplyr)
 
@@ -35,57 +33,19 @@ data$Month <- as.Date(paste(data$Month,"-01",sep=""))
 data$Coal <- as.numeric(data$Coal)
 
 # CREATE TRAINING/TEST SETS ############################################################################
-train <- data[data$Month < "1959-01-01",]
-test <- data[data$Month >= "1959-01-01",]
+train <- data[data$Month < "1959-02-01",]
+test <- data[data$Month >= "1959-02-01",]
 
-# train_series <- ts(train$Coal, start=c(1952, 1), end=c(1958, 12), frequency=12)
-# test_series <- ts(test$Coal, start=c(1959, 1), end=c(1959, 12), frequency=12)
+# GENERATE PREDICTIONS ############################################################################
 
-# # Seasonal decomposition
-# fit <- stl(train_series, s.window="period")
-# plot(fit)
-
-# # additional plots
-# monthplot(train_series)
-# seasonplot(train_series)
-
-# CREATE FITS BASED ON:
-# HOLTWINTERS
-# ARIMA
-
-# # simple exponential - models level
-# fit <- HoltWinters(train_series, beta=FALSE, gamma=FALSE)
-# # double exponential - models level and trend
-# fit <- HoltWinters(train_series, gamma=FALSE)
-# # triple exponential - models level, trend, and seasonal components
-# fit <- HoltWinters(train_series)
-# 
-# # predictive accuracy
-# library(forecast)
-# accuracy(fit)
-# 
-# # predict next three future values
-# library(forecast)
-# forecast(fit, 3)
-# plot(forecast(fit, 3))
-
-# cols: date, actual 1959, prediction 1, pred 2, pred 3
-#df <- data.frame(matrix(ncol = 5, nrow = 0))
-#x <- c("Date", "Actual", "HW Simple", "HW Double", "HW Triple")
-
-
-# df <- data.frame(Date=character(),
-#                  Actual=numeric(), 
-#                  HW_Simple=numeric(), 
-#                  HW_Double=numeric(),
-#                  HW_Triple=numeric(),
-#                  stringsAsFactors = FALSE) 
-
-Date=character(3)
-Actual=numeric(3)
-HW_Simple=numeric(3)
-ARIMA=numeric(3)
-ETS_STL=numeric(3)
+Date=character()
+Actual=numeric()
+HW_Simple=numeric()
+HW_Simple_Error=numeric()
+ARIMA=numeric()
+ARIMA_Error=numeric()
+ETS_STL=numeric()
+ETS_STL_Error=numeric()
 
 count <- 1
 months <- c("February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
@@ -107,22 +67,43 @@ for (month in months){
   # ARIMA
   fit2 <- auto.arima(train_series)
   forecast2 <- as.data.frame(forecast(fit2, 1))
-  pred2 <- forecast1[1,1]
+  pred2 <- forecast2[1,1]
   
   # ETS/STL
   #fit3 <- HoltWinters(train_series)
   forecast3 <- as.data.frame(forecast(train_series, 1))
-  pred3 <- forecast1[1,1]
+  pred3 <- forecast3[1,1]
   
   Date[count] <- paste(m, "1959", sep=" ")
-  Actual[count] <- test[count+1, "Coal"]
+  Actual[count] <- test[count, "Coal"]
   HW_Simple[count] <- pred1
+  HW_Simple_Error[count] <- (pred1 - test[count, "Coal"])/pred1*100
   ARIMA[count] <- pred2
+  ARIMA_Error[count] <- (pred2 - test[count, "Coal"])/pred2*100
   ETS_STL[count] <- pred3
+  ETS_STL_Error[count] <- (pred3 - test[count, "Coal"])/pred3*100
  
   #date_val <- as.character(list(paste(month, "1959", sep=" ")))
   #df <- rbind(df, list(paste(m, "1959", sep=" "),test[count, "Coal"], pred1, pred2, pred3))
   count <- count + 1
 }
 
-df <- data.frame(Date, Actual, HW_Simple, ARIMA, ETS_STL)
+df <- data.frame(Date, Actual, HW_Simple, HW_Simple_Error, ARIMA, ARIMA_Error, ETS_STL, ETS_STL_Error)
+
+# PLOT PREDICTIONS ############################################################################
+
+par(mfrow=c(2,2))
+test_series <- ts(test$Coal, start=c(1959, 2), end=c(1959, 12), frequency=12)
+hw_ts <- ts(df$HW_Simple, start=c(1959, 2), end=c(1959, 12), frequency=12)
+arima_ts <- ts(df$ARIMA, start=c(1959, 2), end=c(1959, 12), frequency=12)
+ets_ts <- ts(df$ETS_STL, start=c(1959, 2), end=c(1959, 12), frequency=12)
+
+ts.plot(test_series, hw_ts, gpars = list(col = c("black", "red")))
+title("Simple Holt-Winters Prediction")
+
+ts.plot(test_series, arima_ts, gpars = list(col = c("black", "red")))
+title("ARIMA Prediction")
+
+ts.plot(test_series, ets_ts, gpars = list(col = c("black", "red")))
+title("ETS Prediction")
+
